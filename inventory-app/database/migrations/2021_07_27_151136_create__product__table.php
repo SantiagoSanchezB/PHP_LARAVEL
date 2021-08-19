b88ae38fd37e1abcb4727461b8939846991d89db
SantiagoB <dssanchez752@misena.edu.co>
2021-08-19 08:15:54 -0500

    Commit of the project inventory-app

---
+++ inventory-app/database/migrations/2021_07_27_151136_create__product__table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('Name',50);
+            $table->double('Cost',10,2);
+            $table->double('Price',10,2);
+            $table->integer('quantity');
+            $table->foreignId('Brand_id',50);
+            $table->timestamps();
+
+            $table->foreign('Brand_id')->references('id')->on('brands');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
