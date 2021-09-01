498c40deeab7813a6d7d459819a8e1aaec62e832
SantiagoS <dssanchez752@misena.edu.co>
2021-09-01 11:53:27 -0500

    Agregate Seeder User and password

--- inventory-app/database/seeders/DatabaseSeeder.php
+++ inventory-app/database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
         $this -> call([
             BrandSeeder::class,
             CategoriesSeeder::class,
-            ProductSeeder::class
+            ProductSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
